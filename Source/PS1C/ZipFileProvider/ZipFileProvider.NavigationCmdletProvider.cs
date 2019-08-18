53ae48c6788dda1bbd1ce869674da3c596dd53ad
Juan Romero <romero126@gmail.com>
2019-08-17 22:42:49 -0700

    Code Refactor

--- Source/PS1C/ZipFileProvider/ZipFileProvider.NavigationCmdletProvider.cs
+++ Source/PS1C/ZipFileProvider/ZipFileProvider.NavigationCmdletProvider.cs
@@ -1,7 +1,8 @@
 using System;
 using Microsoft.PowerShell.Commands;
-
+using System.Collections.Generic;
 using System.IO;
+
 using System.IO.Compression;
 
 using System.Management.Automation;
@@ -22,31 +23,28 @@ namespace PS1C
 
 		protected override bool ItemExists(string path)
 		{
-            //WriteVerbose($"[TODO] ItemExists(string path) '{path}'");
+            //WriteWarning($"[TODO] ItemExists(string path) '{path}'");
             path = NormalizePath(path);
+            //WriteWarning($"[TODO] ItemExists(string path) '{path}'");
 
             if (path == "")
             {
                 return true;
             }
-
-            try {
-                using (ZipArchive zipArchive = ZipFile.OpenRead(PSDriveInfo.Root) )
-                {
-                    ZipArchiveEntry zipArchiveEntry = zipArchive.GetEntry(path);
-                    if (zipArchiveEntry != null) {
-                        return true;
-                    }
-                }
-            }
-            catch (Exception e) {
-                throw e;
+            // ZipFileItemInfo results = new ZipFileItemInfo(PSDriveInfo, path);
+            List<ZipFileItemInfo> results = ZipFileItemInfo.GetFileItemInfo(PSDriveInfo, path);
+            
+            if (results != null)
+            {
+                return true;
             }
             return false;
 		}
 		protected override bool IsItemContainer(string path)
 		{
             path = NormalizePath(path);
+            // WriteWarning($"[TODO] IsItemContainer(string path) '{path}'");
+
             if ( path == "" )
             {
                 return true;
@@ -57,44 +55,49 @@ namespace PS1C
 		protected override void GetChildItems(string path, bool recurse)
 		{
             path = NormalizePath(path);
+
+            if (String.IsNullOrEmpty(path))
+            {
+                path = "*";
+            }
+            // WriteWarning($"[TODO] GetChildItems(string path) '{path}'");            
+
+
             //ZipFileDirectoryInfo DirInfo = new ZipFileDirectoryInfo(PSDriveInfo);
-            using (ZipArchive zipArchive = ZipFile.OpenRead(PSDriveInfo.Root)) {
-				foreach (ZipArchiveEntry zipArchiveEntry in zipArchive.Entries) {
-                    ZipFileItemInfo OutputItem = new ZipFileItemInfo(zipArchiveEntry, PSDriveInfo);
-                    WriteItemObject(OutputItem, OutputItem.FullName, true);
-				}
-			}
+            //using (ZipArchive zipArchive = ZipFile.OpenRead(PSDriveInfo.Root)) {
+			//	foreach (ZipArchiveEntry zipArchiveEntry in zipArchive.Entries) {
+            //        ZipFileItemInfo OutputItem = new ZipFileItemInfo(zipArchiveEntry, PSDriveInfo);
+            //        WriteItemObject(OutputItem, OutputItem.FullName, true);
+			//	}
+			//}
 
-		}
-
-        protected override void GetItem(string path)
-		{
-            string filepath = path.Remove(0, PSDriveInfo.Root.Length+1);
-
-            try {
-                using (ZipArchive archive = ZipFile.OpenRead(PSDriveInfo.Root)) {
-                    ZipArchiveEntry entry = archive.GetEntry(filepath);
-                    ZipFileItemInfo OutputItem = new ZipFileItemInfo(entry, PSDriveInfo);
-                    WriteItemObject(OutputItem, OutputItem.FullName, false);
-                }
-            }
-            catch (Exception e) {
-                throw e;
+            List<ZipFileItemInfo> results = ZipFileItemInfo.GetFileItemInfo(PSDriveInfo, path);
+            foreach (ZipFileItemInfo entry in results)
+            {
+                WriteItemObject(entry, entry.FullName, false);
             }
 		}
+
+        //protected override void GetItem(string path)
+		//{
+        //    string filepath = path.Remove(0, PSDriveInfo.Root.Length+1);
+        //
+        //    try {
+        //        using (ZipArchive archive = ZipFile.OpenRead(PSDriveInfo.Root)) {
+        //            ZipArchiveEntry entry = archive.GetEntry(filepath);
+        //            ZipFileItemInfo OutputItem = new ZipFileItemInfo(entry, PSDriveInfo);
+        //            WriteItemObject(OutputItem, OutputItem.FullName, false);
+        //        }
+        //    }
+        //    catch (Exception e) {
+        //        throw e;
+        //    }
+		//}
         internal ZipFileItemInfo GetItemHelper(string path)
 		{
             path = NormalizePath(path);
-
-            try {
-                using (ZipArchive zipArchive = ZipFile.OpenRead(PSDriveInfo.Root)) {
-                    ZipArchiveEntry zipArchiveEntry = zipArchive.GetEntry(path);
-                    return new ZipFileItemInfo(zipArchiveEntry, PSDriveInfo);
-                }
-            }
-            catch (Exception e) {
-                throw e;
-            }
+            
+            return new ZipFileItemInfo(PSDriveInfo, path);
 		}
 
 
@@ -108,6 +111,7 @@ namespace PS1C
                     ZipArchiveEntry zipArchiveEntry = zipArchive.CreateEntry(path);
 
                     ZipFileItemInfo zipFileItemInfo = new ZipFileItemInfo(zipArchiveEntry, PSDriveInfo);
+
                     return zipFileItemInfo;
                     
                 }
@@ -119,22 +123,69 @@ namespace PS1C
         private string NormalizePath(string path)
         {
 
+
             if (path.StartsWith(PSDriveInfo.Root))
             {
                 path = path.Remove(0, PSDriveInfo.Root.Length+1);
             }
             else if (path.StartsWith($"{PSDriveInfo.Name}:") )
             {
-                path = path.Remove(0, PSDriveInfo.Name.Length+1);
+                path = path.Remove(0, PSDriveInfo.Name.Length+2);
             }
-
             path = path.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+
 
             return path;
         }
 
         #endregion
+        protected override bool ConvertPath(
+            string path,
+            string filter,
+            ref string updatedPath,
+            ref string updatedFilter)
+        {
+            // In order to support Wildcards?
+            // WriteWarning($"ConvertPath ({path}, {filter})");
+            
+            // Don't handle full paths, paths that the user is already trying to
+            // filter, or paths they are trying to escape.
+            if ((!string.IsNullOrEmpty(filter)) ||
+                (path.Contains(Path.DirectorySeparatorChar, StringComparison.Ordinal)) ||
+                (path.Contains(Path.AltDirectorySeparatorChar, StringComparison.Ordinal)) ||
+                (path.Contains("`"))
+                //(path.Contains(StringLiterals.DefaultPathSeparator, StringComparison.Ordinal)) ||
+                //(path.Contains(StringLiterals.AlternatePathSeparator, StringComparison.Ordinal)) ||
+                //(path.Contains(StringLiterals.EscapeCharacter))
+                )
+            {
+                Console.WriteLine("ConvertPath Returned False");
+                return false;
+            }
 
+            // We can never actually modify the PowerShell path, as the
+            // Win32 filtering support returns items that match the short
+            // filename OR long filename.
+            //
+            // This creates tons of seemingly incorrect matches, such as:
+            //
+            // *~*:   Matches any file with a long filename
+            // *n*:   Matches all files with a long filename, but have been
+            //        mapped to a [6][~n].[3] disambiguation bucket
+            // *.abc: Matches all files that have an extension that begins
+            //        with ABC, since their extension is truncated in the
+            //        short filename
+            // *.*:   Matches all files and directories, even if they don't
+            //        have a dot in their name
+
+            // Our algorithm here is pretty simple. The filesystem can handle
+            // * and ? in PowerShell wildcards, just not character ranges [a-z].
+            // We replace character ranges with the single-character wildcard, '?'.
+            updatedPath = path;
+            updatedFilter = System.Text.RegularExpressions.Regex.Replace(path, "\\[.*?\\]", "?");
+            // WriteWarning($"ConvertPath ({updatedPath}, {updatedFilter})");
+            return true;
+        }
         
     }
 }
