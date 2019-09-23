fd3b61d5d87de79d56a424109988bf7fd0e3a6b4
Juan Romero <romero126@gmail.com>
2019-09-22 19:30:33 -0700

    Update to Tests.ps1

--- Test.ps1
+++ Test.ps1
@@ -3,20 +3,17 @@
 
 
 
-$ArchiveFile = "$PSScriptRoot\Tests\ZipFile"
-#Import-Module $PSScriptRoot\Source\PS1C\bin\Debug\netcoreapp3.0\ps1c.psd1 -Force
-#New-PSDrive -Name PSProvider -PSProvider ZipFile -root "$ArchiveFile.zip" -ErrorAction "Stop"
-#
-#Get-Item PSProvider:\Test.txt
-#
-#Get-Item PSProvider:\Waffles
-#
-#Remove-PSDrive PSProvider
-
-Write-Host "Default baseline tests from Microsoft Powershell Team" -ForegroundColor Cyan
+# Write-Host "Default baseline tests from Microsoft Powershell Team" -ForegroundColor Cyan
 # These are Default Tests powershell uses modified for PS1C
-# Invoke-Pester Tests\PS1C.Get-Content.Tests.ps1
-Invoke-Pester Tests\PS1C.Set-Content.Tests.ps1
+
+
+#Invoke-Pester Tests\Get-ChildItem.Tests.ps1
+#Invoke-Pester Tests\Get-Item.Tests.ps1
+#Invoke-Pester Tests\New-Item.Tests.ps1
+#Invoke-Pester Tests\Remove-Item.Tests.ps1
+Invoke-Pester .\Tests\Clear-Content.Tests.ps1
+Invoke-Pester Tests\Get-Content.Tests.ps1
+Invoke-Pester Tests\Set-Content.Tests.ps1
 
 #Write-Host "Internal Tests" -ForegroundColor Cyan
 #Invoke-Pester Tests\PS1C.Example.Tests.ps1
