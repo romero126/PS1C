87910360cd0673732daefb94df393bc33efb1bc4
Juan Romero <romero126@gmail.com>
2019-09-24 12:42:14 -0700

    Update to disable CheckUpdate

--- Source/PS1C/resources/PS1C.psm1
+++ Source/PS1C/resources/PS1C.psm1
@@ -1,18 +1,21 @@
 # Checks to see if a new update is available on Gallery
-$CurVersion = Get-Module -Name ps1c -ListAvailable
-$NextVersion = Find-Module -Name PS1C
-if ($CurVersion.Version -lt $NextVersion.Version)
+$CheckUpdate = $false
+if ($CheckUpdate)
 {
-    
-    Write-Host "A new version of PS1C is available." -ForegroundColor Green
-    Write-Host ""
-    Write-Host "Current Version:", $CurVersion.Version -ForegroundColor Yellow
-    Write-Host ""
+    $CurVersion = Get-Module -Name ps1c -ListAvailable
+    $NextVersion = Find-Module -Name PS1C
+    if ($CurVersion.Version -lt $NextVersion.Version)
+    {
+        
+        Write-Host "A new version of PS1C is available." -ForegroundColor Green
+        Write-Host ""
+        Write-Host "Current Version:", $CurVersion.Version -ForegroundColor Yellow
+        Write-Host ""
 
-    Write-Host ("{0} Release Notes:" -f $NextVersion.Version ) -ForegroundColor Green
-    Write-Host $NextVersion.ReleaseNotes
-    
-    Write-Host "Update-Module -Name PS1C" -ForegroundColor Yellow
+        Write-Host ("{0} Release Notes:" -f $NextVersion.Version ) -ForegroundColor Green
+        Write-Host $NextVersion.ReleaseNotes
+        
+        Write-Host "Update-Module -Name PS1C" -ForegroundColor Yellow
+    }
 }
-
 Import-Module "$PSScriptRoot\ps1c.dll"
